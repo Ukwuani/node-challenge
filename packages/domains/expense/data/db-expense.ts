3582819b9378be98c766e1bb4eac67b20f8b32be
ukwuani <ukwuanibarnabas@gmail.com>
2022-02-08 16:38:43 +0100

    TSK 05 #comment added search to the read expenses query #time 10m

--- packages/domains/expense/data/db-expense.ts
+++ packages/domains/expense/data/db-expense.ts
@@ -1,12 +1,23 @@
-import { query as Query } from '@nc/utils/db';
-import { UUID } from '@nc/utils/types';
+import Db from '@nc/utils/db';
 import { addLimitAndOffset, addOrderBy } from './db.helper';
+import { Expenses, GetExpensesRequest } from '../entity';
 
-export function readExpenses(userId: UUID, limit: number, page: number = 1, sort) {
-  return Query(`
+export async function readExpensesV2(request: GetExpensesRequest) {
+  const repository = await Db.getEntityRepository(Expenses);
+  return repository.find({
+    where: { user_id: request.userId },
+  });
+}
+
+export function readExpensesV1({
+  userId, limit, page, sort, search, searchFields,
+}) {
+  return Db.query(`
   SELECT * 
   FROM expenses 
-  WHERE user_id = $1 
+  WHERE user_id = $1 ${(search && searchFields) ? ' OR ' + JSON.parse(searchFields).map(
+    (f) => f + ' iLIKE %' + search + '%'
+  ).toString().replace(/,/g, ' OR ') : ''}
   ${addOrderBy(sort)}
   ${addLimitAndOffset(limit, page)}`,
   [userId]).then((response) => response.rows);
